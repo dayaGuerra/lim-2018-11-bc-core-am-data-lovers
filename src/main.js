98d7ab18b83e146c104ed8febffa9e21c6c3a6c0
dayaGuerra <dayamandalaman@gmail.com>
2018-12-20 20:14:01 -0500

    El DOM del archivo, funciones para el filtrado por año, se muestra data por año seleccionado

--- src/main.js
+++ src/main.js
@@ -1,47 +1,56 @@
-//slideshow automatic
-var myIndex = 0;
-carousel();
+/* eslint-disable no-undef */
+// HTML - variables del DOM
+const selectDocumentYear = document.getElementById('select_year');
+// const selectDocumentMinYear = document.getElementById('select_minyear');
+// const selectDocumentMaxYear = document.getElementById('select_maxyear');
 
-function carousel() {
-   
-    const mySlides = document.getElementsByClassName("my_slides");
-    for (let i = 0; i < mySlides.length; i++) {
-       mySlides[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > mySlides.length) {myIndex = 1}    
-    mySlides[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 4000); // cambia cada 4 segundos.
+// Data del archivo data.js
+/* Copia de la data Original*/
+const newData = cambiarPropiedad(INJURIES); // array de objetoos
+const years = filtrarPropiedadEspecifica(newData, 'Year'); // Array solo con los valores de la propiedad Year
+
+// DOM para mostrar data en pantalla
+const filterForYear = document.getElementById('filter_years');
+
+// Muestra años en las casillas del select con su respectivo valor.
+
+const mostrarCasillasEnSelect = (array) => {
+  let recibirArreglo = '';
+  array.forEach((ele) => {
+    recibirArreglo += `<option value = "${ele}">${ele}</option>`;
+  }); 
+  return recibirArreglo;
 };
+selectDocumentYear.innerHTML = mostrarCasillasEnSelect(years);
 
-//fin de slideshow automatic
-
-//inicio de botones - Filtrado por 9 categorias del dropdown
-
-
-const btnUrban = document.getElementById("total_urban");
-const btnAir = document.getElementById("total_air");
-
-
-
-//urbano
-btnUrban.addEventListener('click',() => {
-if (elemento.style.display === 'none') {
-elemento.style.display = 'block';
-airFilter.style.display = 'none';
-} 
-else {
-elemento.style.display = 'none';
-}
-});
-
-//aereo
-btnAir.addEventListener('click',() => {
-if (airFilter.style.display === 'none') {
-elemento.style.display = 'none'
-airFilter.style.display = 'block';
-} 
-else {
-airFilter.style.display = 'none';
-}
+selectDocumentYear.addEventListener('change', (event) => {
+  const result = filtro(newData, (parseInt(event.target.value)));
+  const listarItems = (array) => {
+    let cadena = '';
+    array.forEach((obj) => { 
+      cadena += `
+        <div class = "card-year">
+           <article class = "prop-year">
+            <h2 class = "filter-year">${obj.Year}</h2>
+            <div class = "total-heridos-por-año"/>
+                <div> General por año: <span class="info"> ${obj.Total_Injured_Persons}</span></div>
+                <div> En aire: ${obj.Total_Injured_Persons_Air}</div>
+                <div> Ocupantes de bus: ${obj.Total_Injured_Persons_Bus_Occupants}</div>
+                <div> Personas lesionadas en carretera: ${obj.Total_Injured_Persons_Highwayr}</div>
+                <div> En motocicleta: ${obj.Total_Injured_Persons_Motorcyclists}</div>
+                <div> Ocupantes en automóviles: ${obj.Total_Injured_Persons_Passenger_Car_Occupants}</div>
+                <div> Pasajeros de automóviles: ${obj.Total_Injured_Persons_Passenger_Or_Occupant}</div>
+                <div> Peatones heridos: ${obj.Total_Injured_Persons_Pedestrians}</div>
+                <div> Personas lesionadas en aerolíneas de EEUU: ${obj.Total_Injured_Persons_US_Air_Carrier}</div>    
+            </div>
+            </article>
+        </div>
+        `;
+    });
+    
+    return cadena;
+  };
+  
+  
+  filterForYear.innerHTML = listarItems(result);
 });
