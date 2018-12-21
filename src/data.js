af5d29367f5e00dd50cd0a483a83eeb74d4fabc2
cheredia10 <cheredia.colegio@gmail.com>
2018-12-20 19:41:30 -0500

    Aplicando estilos a los templates

--- src/data.js
+++ src/data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // [1960,1965,1970]
 // colocar en un select o como opciones de busqueda en el select.
 // meter en un input?
@@ -19,83 +18,10 @@ const cambiarPropiedad = (data) => {
       return ele.Year === parseInt(inputUs);
     });
   };
-  // filtrar Años dentro de un select - requisito 2
-  window.injuries = {
-    
-    cambiarPropiedad,
-    filtrarPropiedadEspecifica,
-    recib,
-    
-  };
-=======
-const buttonTabla = document.getElementById("tabla_general");
-const tabla = document.getElementById ("tablegeneric");
-
-buttonTabla.addEventListener('click', () => {
-
-const data = INJURIES;
-
-//creo la funcion years, con dos parametros para llamar el indice del array
-function years(item,index) {
-const fullname = parseInt([item.Year]);
-return fullname;
-}
-
-function totalinjuries (item,index) {
-    const tInjuries = [item.Total_Injured_Persons];
-    return tInjuries;
-}
-
-function tpersonInjucar (item,index) {
-    const tInjuriesCar = [item.Total_Injured_Persons_Passenger_Car_Occupants];
-    return tInjuriesCar;
-}
-
-const vg = data.map(years);
-const ti= data.map(totalinjuries);
-const tc= data.map(tpersonInjucar);
-
-
-tableInfo();
-
-function tableInfo()  {
  
- const tabla   = document.getElementById("tablegeneric"),
-       tbody = document.querySelector("#tablegeneric tbody");
-
- tbody.innerHTML = ' ';
-
-const arrYear = vg,
-       arrHeridos= ti,
-       arrMuertos= tc;
-
- const nCantidadYears = vg.length;
-
- for (let i=0; i < nCantidadYears; i++) {
-
-   const fila = document.createElement("tr");
-
-   const celdaYear = document.createElement ("td"),
-         celdaHeredidos = document.createElement ("td"),
-         celdaMuertos = document.createElement ("td");
-
-   const nodoTextYear = document.createTextNode (arrYear[i]),
-         nodoTextHeridos = document.createTextNode (arrHeridos[i]),
-         nodoTextMuertos = document.createTextNode (arrMuertos[i]);
-
-   celdaYear.appendChild(nodoTextYear);
-   celdaHeredidos.appendChild(nodoTextHeridos);
-   celdaMuertos.appendChild(nodoTextMuertos);
-
-   fila.appendChild(celdaYear);
-   fila.appendChild(celdaHeredidos);
-   fila.appendChild(celdaMuertos);
-
-   tbody.appendChild(fila);
-    }
-  tabla.setAttribute("border", "2");
- }
- })
-
-window.INJURIES = INJURIES;
->>>>>>> 411658c62132851991c326e6f763574371339bff
+window.injuries = {
+  cambiarPropiedad,
+  filtrarPropiedadEspecifica,
+  recib,
+    
+};
--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars 
-// 
-// const main = document.getElementById('main');
-// eslint-disable-next-line no-undef
-const newData = cambiarPropiedad(INJURIES);
-// console.log(newData);
-console.log(filtroUrbano(newData, 'Year'));*/
 const newData = cambiarPropiedad(INJURIES);
 const selectDocument = document.getElementById('select_year');
 const year = filtrarPropiedadEspecifica(newData, 'Year');
@@ -26,3 +18,36 @@ selectDocument.addEventListener('change', () => {
   const kl = selectDocument.options[selectDocument.selectedIndex].value; const nj = recib(newData, kl);
   console.log (nj);
 });
+
+
+//TEMPLATES DE AÑOS
+
+const containerList = document.getElementById ("container-list")
+
+const tempYear = (list) => {
+    let templateListYears = "";
+    list.forEach((newData) => {
+        const blockYear = `
+        <div class = "card-year">
+           <article class = "prop-year">
+            <h2 class = "filter-year">${newData.Year}</h2>
+            <div class = "total-heridos-por-año"/>
+                <div> General por año: <span class="info"> ${newData.Total_Injured_Persons}</span></div>
+                <div> En aire: ${newData.Total_Injured_Persons_Air}</div>
+                <div> Ocupantes de bus: ${newData.Total_Injured_Persons_Bus_Occupants}</div>
+                <div> Personas lesionadas en carretera: ${newData.Total_Injured_Persons_Highwayr}</div>
+                <div> En motocicleta: ${newData.Total_Injured_Persons_Motorcyclists}</div>
+                <div> Ocupantes en automóviles: ${newData.Total_Injured_Persons_Passenger_Car_Occupants}</div>
+                <div> Pasajeros de automóviles: ${newData.Total_Injured_Persons_Passenger_Or_Occupant}</div>
+                <div> Peatones heridos: ${newData.Total_Injured_Persons_Pedestrians}</div>
+                <div> Personas lesionadas en aerolíneas de EEUU: ${newData.Total_Injured_Persons_US_Air_Carrier}</div>    
+            </div>
+            </article>
+        </div>
+        `;
+        templateListYears += blockYear;
+    });
+    containerList.innerHTML = templateListYears;
+}
+
+tempYear(newData);
