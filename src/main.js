04675f993913ae09d40ab1cae3d8926b93dd203e
cheredia10 <cheredia.colegio@gmail.com>
2019-01-04 23:34:08 -0500

    Llamar a la función Ordenar

--- src/main.js
+++ src/main.js
@@ -21,6 +21,8 @@ const showSlides = () => {
 
 showSlides();
 
+// Menu de navegación
+ 
 const openNav = document.getElementById('open_nav');
 openNav.addEventListener('click', () => {
   document.getElementById('mySidenav').style.width = '250px';
@@ -37,16 +39,18 @@ const selectDocumentYear = document.getElementById('select_year');
 const selectDocumentMinYear = document.getElementById('select_min_year');
 const selectDocumentMaxYear = document.getElementById('select_max_year');
 const btnMostrarAños = document.getElementById('btn_mostrar_años');
+const selectDocumentOrder = document.getElementById('order');
 
 // DOM para mostrar data en pantalla
 const filterForYear = document.getElementById('filter_years');
 const filterForRang = document.getElementById('filter_rang');
+const orderYear = document.getElementById('order_years');
 
 // Data del archivo data.js
 
 /* Copia de la data Original*/
-const newData = cambiarPropiedad(INJURIES); // array de objetoos
-const years = filtrarPropiedadEspecifica(newData, 'Year');
+const newData = injuries.cambiarPropiedad(INJURIES); // array de objetoos
+const years = injuries.filtrarPropiedadEspecifica(newData, 'Year');
 
 // Funcion para mostrar cartas solo con los valores de la propiedad Year
 const listarItems = (obje, dive) => {
@@ -74,7 +78,7 @@ const listarItems = (obje, dive) => {
   dive.innerHTML = cadena;
 };
 
-
+// Funcion para mostrar los años en el select
 const mostrarCasillasEnSelect = (array) => {
   let recibirArreglo = '';
   array.forEach((ele) => {
@@ -86,40 +90,30 @@ selectDocumentYear.innerHTML = mostrarCasillasEnSelect(years);
 selectDocumentMinYear.innerHTML = mostrarCasillasEnSelect(years);
 selectDocumentMaxYear.innerHTML = mostrarCasillasEnSelect(years);
 
- 
+// Mostrar cartas según la selección ingresada por el usuario
+
 selectDocumentYear.addEventListener('change', (event) => {
-  const result = filtro(newData, (parseInt(event.target.value)));
+  const result = injuries.filtro(newData, (parseInt(event.target.value)));
   result.innerHTML = listarItems(result, filterForYear);
 }); 
 
- 
+// Botón que muestra el resultado de los dos valores que el usuario ha ingresado
+
 btnMostrarAños.addEventListener('click', () => {
   document.getElementById('page_one').style.display = 'none';
   const minYear = selectDocumentMinYear.value;
   const maxYear = selectDocumentMaxYear.value;
   
-  const respt = filtroMinMax(newData, minYear, maxYear);
+  const respt = injuries.filtroMinMax(newData, minYear, maxYear);
   respt.innerHTML = listarItems(respt, filterForRang);
 });
 
 selectDocumentMinYear.innerHTML = mostrarCasillasEnSelect(years);
 selectDocumentMaxYear.innerHTML = mostrarCasillasEnSelect(years);
 
+// Función para ordenar los datos por años y mostrarlos en pantalla
 
-// function myFunction() {
-//   let input, filter, ul, li, a, i;
-//   input = document.getElementById('mysearch');
-//   filter = input.value.toUpperCase();
-//   ul = document.getElementById('myul');
-//   li = ul.getElementsByTagName('li');
-//   for (i = 0; i < li.length; i++) {
-//     a = li[i].getElementsByTagName('a')[0];
-//     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//       li[i].style.display = '';
-//     } else {
-//       li[i].style.display = 'none';
-//     }
-//   }
-// }
-
-
+selectDocumentOrder.addEventListener('click', () => {
+  const yearOrder = injuries.sorts(newData, event.target.value);
+  listarItems(yearOrder, orderYear);
+});
